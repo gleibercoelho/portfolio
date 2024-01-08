f290519855596e3f8665c5203f6b283f437f6225
GFC <gleiberfelipe@hotmail.com>
2024-01-08 01:16:17 -0300

    final deploy.6

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        
-    <Route path='/*' element={<App/>} />
-    
-    </Routes>
-    </Router>
+    <BrowserRouter>
+        <App />        
+      </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
