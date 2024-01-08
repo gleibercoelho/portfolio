bdd8997544ce0714b68c0483af595eaaf8107e5c
GFC <gleiberfelipe@hotmail.com>
2024-01-07 23:53:59 -0300

    final deploy.1

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.tsx';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Routes>
+        <Switch>
     <Route path='/*' element={<App/>} />
+    </Switch>
     </Routes>
     </Router>
   </React.StrictMode>,
