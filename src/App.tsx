f9cb6eed21d5aca202528c10eff03811b3e1be6b
GFC <gleiberfelipe@hotmail.com>
2024-01-08 00:23:33 -0300

    final deploy.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import { useLocation, Routes, Route } from "react-router-dom";
+
+import { useLocation, useRoutes } from "react-router-dom";
 import Home from "./pages/home/index";
 import About from "./pages/about";
 import Contact from "./pages/contact";
 import Project from "./pages/Project";
 import ProductDetailsPage from "./pages/projects details";
 import { AnimatePresence } from "framer-motion";
+import React from "react";
 
+ export default function App() {
+    const element = useRoutes([
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/about",
+        element: (
+          <About/>
+        )
+      },
+      {
+        path: "//contact",
+        element: (
+          <Contact/>
+        )
+      },
+      {
+        path: "/projects",
+        element: (
+          <Project/>
+        )
+      },
+      {
+        path: "//projects/:productId",
+        element: (
+          <ProductDetailsPage/>
+        )
+      }
+    ]);
+  
+    const location = useLocation();
+  
+    if (!element) return null;
+  
+    return (
+      <AnimatePresence mode="wait">
+        {React.cloneElement(element, { key: location.pathname })}
+      </AnimatePresence>
+    );
 
-export default function App() {
-  const location = useLocation();
-
-  return (
-    <AnimatePresence mode="wait">
-      <Routes location={location}>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/projects" element={<Project />} />
-        <Route path="/projects/:productId" element={<ProductDetailsPage />} />
-      </Routes>
-    </AnimatePresence>
-  );
+ 
 }
