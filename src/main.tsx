f537d914d7c2dae600c24a455146107417c8ef1c
GFC <gleiberfelipe@hotmail.com>
2024-01-08 12:33:55 -0300

    final deploy.11

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/portfolio/">
       <Routes>
     <Route path='/*' element={<App/>} />
     </Routes>
